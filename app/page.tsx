00428a696c170fc49176cea5a01ca1de6f345225
sriniupputuri <srinivasupputuri9999@gmail.com>
2024-01-29 13:01:09 +0530

    added contact page for route practise

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+// use client
+
 import Imageport from "@/components/Imageport";
 import Navbar from "@/components/Navbar";
+import About from "./about/page";
 
 export default function Home() {
   return (
       <div className="h-full">
         <Navbar />
-        {/* hjgj */}
         <Imageport />
+        <About />
       </div>
   )
 }
--- components/Imageport.tsx
+++ components/Imageport.tsx
@@ -1,17 +1,49 @@
+// use client
+"use client";
 import React from "react";
+import Image from "next/image";
+import img from "../Images/srini.jpeg";
 
 const Imageport: React.FC = () => {
   return (
-    <div className="h-full pt-8 bg-white">
-      <div className="h-4/5 flex justify-between p-6">
-        <div className="image">
-          {/* <img src="../public/blob.svg" alt="" /> */}
+    <div className="h-screen pt-20 bg-zinc-100">
+      <div className="sm:flex justify-around p-6">
+        <div className="image flex justify-center">
+          <Image
+            src={img}
+            alt="pet"
+            width={200}
+            height={200}
+            className="rounded-full"
+          />
         </div>
-        <div className="content hidden md:block">content</div>
+        <div className="flex justify-center items-center text-center py-6 sm:py-0">
+          <h2 className="text-slate-500 text-[20px]">
+            {`I'm a`}{" "}
+            <span className="px-2 text-lg font-bold text-[40px] bg-gradient-to-r from-pink-800 to-blue-500 bg-clip-text text-transparent">
+              React Developer
+            </span>{" "}
+          </h2>
+        </div>
       </div>
-      <div>
-        <p>Skills</p>
+      <div className="flex justify-center text-justify gap-4 p-10">
+        <h2 className="font-bold text-cyan-900">Skills: </h2>
+        <ul className="flex flex-wrap gap-6">
+          <li className="text-slate-700">HTML</li>
+          <li className="text-neutral-700">CSS</li>
+          <li className="text-red-700">BOOTSTRAP</li>
+          <li className="text-zinc-700">TAILWIND</li>
+          <li className="text-cyan-700">JAVASCRIPT</li>
+          <li className="text-lime-900">REACT</li>
+          <li className="text-blue-700">REDUX</li>
+          <li className="text-fuchsia-500">REACT-QUERY</li>
+          <li className="text-orange-700">REDUX</li>
+          <li className="text-teal-400">MUI</li>
+          <li className="text-lime-500">NEXT</li>
+          <li className="text-yellow-500">TYPESCRIPT</li>
+        </ul>
       </div>
+      
     </div>
   );
 };
